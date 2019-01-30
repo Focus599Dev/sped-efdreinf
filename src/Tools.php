d2075891b8c4fcd68ef0366ae17e1a13453391e2
Marlon oliveira barbosa <marlon.academi@gmail.com>
2019-01-30 15:26:02 -0200

    Update Tools.php

--- src/Tools.php
+++ src/Tools.php
@@ -52,9 +52,9 @@ class Tools extends ToolsBase
      * @var array
      */
     protected $uriconsulta = [
-        '1' => 'https://reinf.receita.fazenda.gov.br/WsREINF/ConsultasReinf.svc',
-        '2' => 'https://preprodefdreinf.receita.fazenda.gov.br/WsREINF/ConsultasReinf.svc',
-        '3' => 'https://preprodefdreinf.receita.fazenda.gov.br/WsREINF/ConsultasReinf.svc'
+        '1' => ' https://reinf.receita.fazenda.gov.br/WsReinfConsultas/ConsultasReinf.svc',
+        '2' => 'https://preprodefdreinf.receita.fazenda.gov.br/WsReinfConsultas/ConsultasReinf.svc',
+        '3' => 'https://preprodefdreinf.receita.fazenda.gov.br/WsReinfConsultas/ConsultasReinf.svc'
     ];
 
     /**
